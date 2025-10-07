19f919f246bfa6bc75887dbbdcbe6af23e842218
johnb08 <johnb08.kodehode@gmail.com>
2025-10-07 14:45:21 +0200

    init

---
+++ App/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Core.Models;
+
+Console.WriteLine("Hello, World!");
+
+/* Her ser du at vår GenericEntity får vite at den skal jobbe med int datatypen i IntStorer */
+GenericEntity<int> IntStorer = new();
+IntStorer.StoredValue = 10;
+
+/* Og skal jobbe med string datatypen i StringStorer */
+GenericEntity<string> StringStorer = new();
+StringStorer.StoredValue = "Hello, world!";
+
+List<float> floats = [];
+
+/* Målet vårt for uken er å lage en generisk våpenliste, som kan ta inn forskjellige våpen og lagre disse for oss. Nedenfor ser du eksempler på hvor vi vil være tilslutt. */
+
+/* WeaponList<LongSword> longSwords = [];
+
+WeaponList<Hammer> hammers = []; */
---
+++ Core/Models/GenericEntity.cs
@@ -0,0 +1,11 @@
+namespace Core.Models;
+
+/* En generic class er en klasse som ikke vet hva datatype den skal jobbe med, før du faktisk lager et objekt med den. 
+Legg merke til at den tar inn et parameter på en ny måte, via typeparameteret <TStored> TStored representerer datatypen StoredValue jobber med.  */
+public class GenericEntity<TStored>
+{
+    /* Her ser du at vi par en Property StoredValue som jobber med / behandler den ukjente datatypen TStored. 
+    Vår GenericEntity class vet ikke hva TStored er, før etter vi faktisk lager et objekt med klassen. Hvis du går inn i App prosjektet,
+    vil du se at vi lager to forskjellige GenericEntity objekter der, en hvor TStored er int, og en hvor TStored er string. */
+    public TStored StoredValue { get; set; }
+}
