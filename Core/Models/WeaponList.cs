bd66a8c46bd1663aa29c7344423e405600ed0df2
johnb08 <johnb08.kodehode@gmail.com>
2025-10-07 14:46:50 +0200

    comments in weaponlist

--- Core/Models/WeaponList.cs
+++ Core/Models/WeaponList.cs
@@ -2,7 +2,8 @@ using System;
 
 namespace Core.Models;
 
+/* Vårt mål på torsdag er å implementere en listetype vi kaller WeaponList som tar inn, og jobber med typen TWeapon. */
 public class WeaponList<TWeapon>
 {
-
+    /* Her skal vi sette opp måter å lagre store mengder av TWeapons typen, samt måter å behandle de på. */
 }
